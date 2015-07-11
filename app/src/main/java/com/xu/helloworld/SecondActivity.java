435ce530c5b9032f6c65c0a4b64d28e495120812
XU <1790804959@qq.com>
2015-07-11 18:35:32 +0800

    second commit

---
+++ app/src/main/java/com/xu/helloworld/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.xu.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by XU on 2015/7/11.
+ */
+public class SecondActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
